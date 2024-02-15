7f7925a19427471a68b578360318e75ee2e4f7a2
sihun <ohsh0731@gmail.com>
2024-02-15 19:43:31 +0900

    prototype scope

---
+++ src/main/java/com/sihun/springtil/prototypescope/resourcebean/ProcessingService.java
@@ -0,0 +1,17 @@
+package com.sihun.springtil.prototypescope.resourcebean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProcessingService {
+
+    @Autowired
+    private ApplicationContext context;
+
+    public void processTask() {
+        HeavyResourceProcessor processor = context.getBean(HeavyResourceProcessor.class);
+        processor.process();
+    }
+}

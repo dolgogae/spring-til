f1d15a5794adb1aa4405bfff80ad1a9433bb5b2a
sihun oh <ohsh0731@gmail.com>
2024-03-21 19:22:38 +0900

    feat: dir-pjt/direction 서비스 구현

---
+++ project-direction/src/main/java/com/example/projectdirection/direction/service/DirectionService.java
@@ -0,0 +1,59 @@
+package com.example.projectdirection.direction.service;
+
+import com.example.projectdirection.api.dto.DocumentDto;
+import com.example.projectdirection.direction.entity.Direction;
+import com.example.projectdirection.pharmacy.dto.PharmacyDto;
+import com.example.projectdirection.pharmacy.service.PharmacySearchService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class DirectionService {
+
+    private static final int MAX_SEARCH_COUNT = 3; // 최대 검색 갯수
+    private static final double RADIUS_KM = 10.0; // 반경 10 km
+    private static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
+
+    private final PharmacySearchService pharmacySearchService;
+
+    public List<Direction> buildDirectionList(DocumentDto documentDto){
+        if(Objects.isNull(documentDto)) return Collections.emptyList();
+
+        return pharmacySearchService.searchPharmacyDtoList()
+                .stream().map(pharmacyDto ->
+                        Direction.builder()
+                                .inputAddress(documentDto.getAddressName())
+                                .inputLongitude(documentDto.getLongitude())
+                                .inputLatitude(documentDto.getLatitude())
+                                .targetPharmacyName(pharmacyDto.getPharmacyName())
+                                .targetAddress(pharmacyDto.getPharmacyAddress())
+                                .targetLatitude(pharmacyDto.getLatitude())
+                                .targetLongitude(pharmacyDto.getLongitude())
+                                .distance(
+                                        calculateDistance(documentDto.getLatitude(), documentDto.getLongitude(),
+                                                pharmacyDto.getLatitude(), pharmacyDto.getLongitude())
+                                )
+                                .build())
+                .filter(direction -> direction.getDistance() <= RADIUS_KM)
+                .sorted(Comparator.comparing(Direction::getDistance))
+                .limit(MAX_SEARCH_COUNT)
+                .toList();
+
+    }
+
+    // Haversine formula
+    private double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+        lat1 = Math.toRadians(lat1);
+        lon1 = Math.toRadians(lon1);
+        lat2 = Math.toRadians(lat2);
+        lon2 = Math.toRadians(lon2);
+
+        double earthRadius = 6371; //Kilometers
+        return earthRadius * Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
+    }
+}
---
+++ project-direction/src/main/java/com/example/projectdirection/pharmacy/service/PharmacySearchService.java
@@ -0,0 +1,34 @@
+package com.example.projectdirection.pharmacy.service;
+
+import com.example.projectdirection.pharmacy.dto.PharmacyDto;
+import com.example.projectdirection.pharmacy.entity.Pharmacy;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PharmacySearchService {
+
+    private final  PharmacyRepositoryService pharmacyRepositoryService;
+
+    public List<PharmacyDto> searchPharmacyDtoList(){
+        // redis
+
+        // db
+        return pharmacyRepositoryService.findAll().stream().map(this::convertToPharmacyDto).toList();
+    }
+
+    private PharmacyDto convertToPharmacyDto(Pharmacy pharmacy ){
+        return PharmacyDto.builder()
+                .id(pharmacy.getId())
+                .pharmacyAddress(pharmacy.getPharmacyAddress())
+                .pharmacyName(pharmacy.getPharmacyName())
+                .latitude(pharmacy.getLatitude())
+                .longitude(pharmacy.getLongitude())
+                .build();
+    }
+}

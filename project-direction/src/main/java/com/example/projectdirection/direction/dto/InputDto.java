8f0170952b5a29ac4b6771aee1c83fb770b7e6fc
sihun oh <ohsh0731@gmail.com>
2024-03-24 20:11:11 +0900

    feat: 약국 길찾기 완성

---
+++ project-direction/src/main/java/com/example/projectdirection/direction/dto/InputDto.java
@@ -0,0 +1,11 @@
+package com.example.projectdirection.direction.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class InputDto {
+
+    private String address;
+}
--- project-direction/src/main/java/com/example/projectdirection/pharmacy/service/PharmacyRecommendationService.java
+++ project-direction/src/main/java/com/example/projectdirection/pharmacy/service/PharmacyRecommendationService.java
@@ -3,13 +3,17 @@ package com.example.projectdirection.pharmacy.service;
 import com.example.projectdirection.api.dto.DocumentDto;
 import com.example.projectdirection.api.dto.KakaoApiResponseDto;
 import com.example.projectdirection.api.service.KakaoAddressSearchService;
+import com.example.projectdirection.direction.dto.OutputDto;
 import com.example.projectdirection.direction.entity.Direction;
 import com.example.projectdirection.direction.service.DirectionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -21,18 +25,44 @@ public class PharmacyRecommendationService {
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
 
-    public void recommendPharmacyList(String address){
+    private static final String ROAD_VIEW_BASE_URL = "https://map.kakao.com/link/roadview/";
+    private static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
+
+    @Value("${pharmacy.recommendation.base.url}")
+    private String baseUrl;
+
+    public List<OutputDto> recommendPharmacyList(String address){
 
         KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
 
         if(Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentDtoList())){
             log.error("[PharmacyRecommendationService recommendPharmacyList] Input address: {}", address);
-            return;
+            return Collections.emptyList();
         }
 
         DocumentDto documentDto = kakaoApiResponseDto.getDocumentDtoList().get(0);
 
         List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
-        directionService.saveAll(directionList);
+        return directionService.saveAll(directionList).stream()
+                .map(this::convertToOutputDto)
+                .toList();
+    }
+
+    private OutputDto convertToOutputDto(Direction direction){
+
+        String params = String.join(",", direction.getTargetPharmacyName(),
+                String.valueOf(direction.getTargetLatitude()), String.valueOf(direction.getTargetLongitude()));
+
+        String result = UriComponentsBuilder.fromHttpUrl(DIRECTION_BASE_URL + params).toUriString();
+
+        log.info("direction params: {}, url: {}", params, result);
+
+        return OutputDto.builder()
+                .pharmacyAddress(direction.getTargetAddress())
+                .pharmacyName(direction.getTargetPharmacyName())
+                .directionUrl(result)
+                .roadViewUrl(ROAD_VIEW_BASE_URL + direction.getTargetLatitude() + ","   + direction.getTargetLongitude())
+                .distance(String.format("%.2f km", direction.getDistance()))
+                .build();
     }
 }

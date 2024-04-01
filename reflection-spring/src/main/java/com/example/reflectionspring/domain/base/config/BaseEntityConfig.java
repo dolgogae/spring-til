38015d3592cd034921f73098add6c423edbad417
sihun <ohsh0731@gmail.com>
2024-04-01 23:05:41 +0900

    Revert "feat: spring advence project setting"
    This reverts commit f5d07802cf6d86ed445d51a627ea97f3cb3bc85e.

--- reflection-spring/src/main/java/com/example/reflectionspring/domain/base/config/BaseEntityConfig.java
+++ reflection-spring/src/main/java/com/example/reflectionspring/domain/base/config/BaseEntityConfig.java
@@ -1,21 +1,7 @@
 package com.example.reflectionspring.domain.base.config;
 
-import com.example.reflectionspring.domain.base.mapper.BaseEntityMapper;
-import com.example.reflectionspring.domain.base.service.BaseEntityService;
-import com.example.reflectionspring.domain.entity1.data.Entity1;
-import com.example.reflectionspring.domain.entity1.data.Entity1Part;
-import com.example.reflectionspring.domain.entity1.dto.Entity1PartDto;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BaseEntityConfig {
-
-    @Bean(name = "entity1BaseService")
-    public BaseEntityService<Entity1, Entity1Part, Entity1PartDto> entity1BaseService(){
-        return new BaseEntityService<>(
-                new BaseEntityMapper<>(Entity1.class, Entity1Part.class, Entity1PartDto.class),
-                Entity1.class, Entity1Part.class, Entity1PartDto.class
-        );
-    }
 }

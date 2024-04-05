5546f351f9eb0eca43a0848efaffe54e37b696b9
sihun <ohsh0731@gmail.com>
2024-04-06 08:47:59 +0900

    feat: template method 소스코드 적용

--- reflection-spring/src/test/java/com/example/reflectionspring/domain/entity1/repository/Entity1RepositoryTest.java
+++ reflection-spring/src/test/java/com/example/reflectionspring/domain/entity1/repository/Entity1RepositoryTest.java
@@ -1,14 +1,17 @@
 package com.example.reflectionspring.domain.entity1.repository;
 
 import com.example.reflectionspring.ReflectionSpringApplication;
+import com.example.reflectionspring.domain.base.dto.BaseCreateDto;
 import com.example.reflectionspring.domain.entity1.data.Entity1;
+import com.example.reflectionspring.domain.entity1.dto.Entity1PartDto;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,5 +26,17 @@ class Entity1RepositoryTest {
 
     @Test
     void insertTest(){
+        // given
+        BaseCreateDto<Entity1PartDto> dto = BaseCreateDto.<Entity1PartDto>builder()
+                .parentId(parentId)
+                .parts(new ArrayList<>())
+                .build();
+        Entity1 entity1 = new Entity1(dto);
+
+        // when
+        Entity1 savedEntity = entity1Repository.insert(entity1);
+
+        entity1Repository.insert(savedEntity);
+        // then
     }
 }

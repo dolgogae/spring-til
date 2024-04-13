c086a413a82220918e48ff9aae704e97a6a09a26
sihun <ohsh0731@gmail.com>
2024-04-13 19:36:44 +0900

    feat: example project setting

---
+++ sping-core2-inflearn/proxy/src/main/java/hello/proxy/app/v2/OrderRepositoryV2.java
@@ -0,0 +1,21 @@
+package hello.proxy.app.v2;
+
+import hello.proxy.app.v1.OrderRepositoryV1;
+
+public class OrderRepositoryV2 {
+    public void save(String itemId) {
+        // 저장로직
+        if (itemId.equals("ex")){
+            throw new IllegalStateException("예외 발생!");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(int i) {
+        try {
+            Thread.sleep(i);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}

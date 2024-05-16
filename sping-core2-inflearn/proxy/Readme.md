5ecb1130ead7c8adbda7950a663c0d8694483d1a
sihun <ohsh0731@gmail.com>
2024-05-16 20:02:49 +0900

    feat: proxy factory

---
+++ sping-core2-inflearn/proxy/Readme.md
@@ -0,0 +1,9 @@
+# Proxy
+- TheadLocal 사용
+- 디자인 패턴
+   > TemplateMethod
+   >  
+- Proxy Factory Pattern
+    > `/src/test/**/common/advice/*`  
+     `/src/test/**/proxyfactory/*`
+
---
+++ sping-core2-inflearn/proxy/src/test/java/hello/proxy/common/advice/TimeAdvice.java
@@ -0,0 +1,27 @@
+package hello.proxy.common.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+@Slf4j
+public class TimeAdvice implements MethodInterceptor {
+
+    /**
+     * target클래스 정보는 이미 invocatoin 안에 모두 포함됐다.
+     * 프록시 팩토리를 생성하는 단계에서 target 정보를 파라미터로 전달받는다.
+     */
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+//        Object result = method.invoke(target, args);
+        Object result = invocation.proceed();
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime={}", resultTime);
+        return result;
+    }
+}
